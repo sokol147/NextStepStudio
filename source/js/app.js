83cadcd819210364d1819bfe9a34e39e205e9698
Sokol V <1470715@gmail.com>
2017-10-11 23:50:03 +0300

    add tabs

--- source/js/app.js
+++ source/js/app.js
@@ -6,3 +6,16 @@ document.body.onload = function(){
 		}
 	}, 4700);
 }
+
+$(document).ready(function(){
+	$('.panel__link').on('click', function(e){
+		e.preventDefault();
+
+		var item = $(this).closest('.panel'),
+				contentItem = $('.project__content'),
+				itemPosition = item.index();
+
+		contentItem.eq(itemPosition)
+			.addClass('active').siblings().removeClass('active');
+	})
+});
